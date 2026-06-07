c588fcf2ed8db009b1139fad35e22453a14546da
Mrc Network official <mrcnetwork10@gmail.com>
2026-06-08 01:32:30 +0300

    Refactor bot creation and event handling

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const mineflayer = require('mineflayer')
 
 function createBot() {
+  const bot = mineflayer.createBot({
+    host: '162.55.241.186',
+    port: 12723,
+    username: 'DistractBot',
+    version: '1.20.1'
+  })
 
-const bot = mineflayer.createBot({
-  host: '162.55.241.186',
-  port: 12733,
-  username: 'DistractBot',
-  version: '1.20.1'
-})
+  bot.on('login', () => {
+    console.log('Bot Logged In')
+  })
 
-bot.on('spawn', () => {
-  console.log('Bot Joined')
+  bot.on('spawn', () => {
+    console.log('Bot Joined Server')
 
-  setInterval(() => {
+    setInterval(() => {
+      bot.setControlState('forward', true)
 
-    bot.setControlState('forward', true)
+      setTimeout(() => {
+        bot.setControlState('forward', false)
+      }, 2000)
+    }, 10000)
+  })
 
-    setTimeout(() => {
-      bot.setControlState('forward', false)
-    }, 2000)
+  bot.on('end', () => {
+    console.log('Disconnected - Reconnecting in 5s...')
+    setTimeout(createBot, 5000)
+  })
 
-  }, 10000)
-})
+  bot.on('kicked', (reason) => {
+    console.log('Kicked:', reason)
+  })
 
-bot.on('end', () => {
-  console.log('Reconnecting...')
-  setTimeout(createBot, 5000)
-})
-
-bot.on('error', console.log)
+  bot.on('error', (err) => {
+    console.log('Error:', err.message)
+  })
 }
 
 createBot()
